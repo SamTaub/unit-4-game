7474134b619710cd53604ef4918c39b43e2244bb
Sam Taub <staub91@gmail.com>
2018-11-26 15:23:33 -0500

    added game logic

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+// //Variable List
+// var minMagicNumber = 19;
+// var maxMagicNumber = 120;
+// var magicNumber = createRandomNumber (minMagicNumber, maxMagicNumber);
+
+// //Function that creates a random number from 19-120
+// function createRandomNumber (min, max){
+//     return Math.floor(Math.random()*(max-min+1)+min);
+// }
+
+
+// $("#game-magic-number").text(magicNumber);
